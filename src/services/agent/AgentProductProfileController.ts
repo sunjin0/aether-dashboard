9ddc1765e8b5805243f10f3def8d03281e870f88
孙进 <2367283463@qq.com>
2026-08-26 16:31:58 +0800

    feat: 支持创建Agent产品发布草稿

--- src/services/agent/AgentProductProfileController.ts
+++ src/services/agent/AgentProductProfileController.ts
@@ -5,3 +5,4 @@ export const getAgentProductProfiles = (applicationId?: string) => request<Respo
 export const createAgentProductProfile = (data: Omit<AgentProductProfile, 'id' | 'status' | 'versionNo'>) => request<ResponseStructure<string>>('/api/agent/product-profile', { method: 'POST', data })
 export const updateAgentProductProfile = (id: string, data: Omit<AgentProductProfile, 'id' | 'status' | 'versionNo'>) => request<ResponseStructure<void>>(`/api/agent/product-profile/${id}`, { method: 'PUT', data })
 export const publishAgentProductProfile = (id: string) => request<ResponseStructure<AgentProductProfile>>(`/api/agent/product-profile/${id}/publish`, { method: 'POST' })
+export const copyAgentProductProfile = (id: string) => request<ResponseStructure<string>>(`/api/agent/product-profile/${id}/copy`, { method: 'POST' })
